720501e25f9a190aca345be320c19650c22aa27d
azharrizki <azharrizki7834@gmail.com>
2021-03-02 23:02:52 +0700

    new portofolio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function showNav() {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
